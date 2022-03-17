5fb3cf5b3f233de3e2a83c3a568a63c5855303db
jclong <j.mail1014@gmail.com>
2022-03-17 16:52:18 -0400

    set up folder and basic file structure

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <section>
+            <h1>Jessica Long | Front End Web Developer</h1>
+        </section>
+    );
+}
+
+export default Header;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">About Me</a>
+          </li>
+          <li>
+            <a href="#">Portfolio</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
