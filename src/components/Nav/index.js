5504d820337ae9a248eaf44e6ebfd33098b90914
jclong <j.mail1014@gmail.com>
2022-03-19 14:01:55 -0400

    add nav component with navlinks from react-router-dom

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,25 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">About Me</a>
-          </li>
-          <li>
-            <a href="#">Portfolio</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <a href="#">Resume</a>
-          </li>
-        </ul>
+    <div className="navigation">
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">
+            Jessica Long | Front End Web Developer
+          </NavLink>
+          <div>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  About me
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/portfolio">
+                  Portfolio
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contact">
+                  Contact
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/resume">
+                  Resume
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
-    </header>
+    </div>
   );
 }
 
