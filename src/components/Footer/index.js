4a99712bc140685a87cbb673f92a399201e5254d
jclong <j.mail1014@gmail.com>
2022-03-19 14:18:26 -0400

    add footer and icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-function Footer() {}
+function Footer() {
+  return (
+    <div className="footer">
+      <footer>
+        <div class="container">
+        <i class="fab fa-github"></i>
+        <i class="fab fa-linkedin-in"></i>
+        <i class="fab fa-twitter"></i>
+        </div>
+      </footer>
+    </div>
+  );
+}
 
 export default Footer;
